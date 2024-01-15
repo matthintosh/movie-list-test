113cfdf3293a6d15df1ce8613f7901c87d806463
Matthieu Rocher <matthieu@taketurns.global>
2024-01-15 15:11:41 +0100

    fix: Issue on generated movie interface

--- apps/api/src/app/movie/movie.module.ts
+++ apps/api/src/app/movie/movie.module.ts
@@ -5,13 +5,16 @@ import { join } from 'path';
 import { MovieResolver } from './movie.resolver';
 import { MovieService } from './movie.service';
 
+const test =  process.cwd()
+console.debug(test)
+
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       typePaths: ['./**/*.graphql'],
       definitions: {
-        path: join(process.cwd(), './movie/generated/movie.ts'),
+        path:  join(process.cwd(), 'apps/api/src/app/movie/generated/movie.ts'),
       },
     }),
   ],
