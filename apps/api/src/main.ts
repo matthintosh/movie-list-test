9fa8fa51e86be070f3f948d1fa706bc7581ff925
Matthieu Rocher <matthieu@taketurns.global>
2024-01-31 14:13:20 +0100

    fix : missing enable cors for local testing

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const globalPrefix = 'graphql';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  app.enableCors()
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
